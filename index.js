665e5e38294e8428a93574793536900c4d49085c
Jemima Abu <jemimaabu@gmail.com>
2019-01-25 21:12:40 +0100

    Actually styled this one yay

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 var listInput = document.getElementById("list-input");
 var sortedList = document.getElementById("sorted-list");
+var listInput = document.getElementById("list-input");
+var separateBy;
+var displayBy;
+var sortUsing;
+var sortOrder;
+
+function seperateMethod() {
+
+}
+
+function displayMethod() {
+
+}
+
+function sortUsingMethod() {
+
+}
+
+function sortOrderMethod() {
+    
+}
 
 function listSort() {
     var input = listInput.value;
     var inputArray = input.split(/\n/g);
-    sortedList.innerHTML = inputArray.sort().join("<br />");
+    sortedList.innerHTML = inputArray.filter(x => (x !== "")).sort().join("<br />");
     console.log(inputArray)
 }
